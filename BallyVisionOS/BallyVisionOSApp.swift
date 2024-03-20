74dd1c1d6b99f0a0d8df1e2c2989d87377e2368a
rafael-quixabeira <89958713+rafael-quixabeira@users.noreply.github.com>
2024-03-20 17:40:51 -0300

    Initial Commit

---
+++ BallyVisionOS/BallyVisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallyVisionOSApp.swift
+//  BallyVisionOS
+//
+//  Created by Rafael Quixabeira on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct BallyVisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
